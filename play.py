7ef3ab2a901503425e1d8adc20cfb96a9d647a14
jzybert <jmzybert@gmail.com>
2017-11-25 10:38:07 -0800

    command line arguments
    -p or --play to play a normal game of chess

--- play.py
+++ play.py
@@ -1,23 +1,29 @@
+import argparse
 from chess import *
 from pieces import *
 
-game = ChessGame()
+parser = argparse.ArgumentParser(description="Options for a chess game.")
+parser.add_argument("-p", "--play", help="play a game of chess", action="store_true")
+args = parser.parse_args()
 
-startColor = input("Which color is starting first (white or black): ")
-colorsToMove = [Color.WHITE, Color.BLACK]
-colorIndex = 0
-if startColor == "white":
+if (args.play):
+	game = ChessGame()
+	
+	startColor = input("Which color is starting first (white or black): ")
+	colorsToMove = [Color.WHITE, Color.BLACK]
 	colorIndex = 0
-else:
-	colorIndex = 1
-
-colors = ["White", "Black"]
-while not game.isOver():
-	game.printBoard()
-	print(colors[colorIndex % 2] + ", it's your move.")
-	moveFrom = tuple(int(x.strip()) for x in input("Which piece would you like to move?: ").split(","))
-	moveTo = tuple(int(x.strip()) for x in input("Where would you like to move that piece?: ").split(","))
-	if game.movePiece(moveFrom, moveTo, colorsToMove[colorIndex]):
-		colorIndex += 1
+	if startColor == "white":
+		colorIndex = 0
 	else:
-		print("That was not a vailid move. Try again.")
+		colorIndex = 1
+	
+	colors = ["White", "Black"]
+	while not game.isOver():
+		game.printBoard()
+		print(colors[colorIndex % 2] + ", it's your move.")
+		moveFrom = tuple(int(x.strip()) for x in input("Which piece would you like to move?: ").split(","))
+		moveTo = tuple(int(x.strip()) for x in input("Where would you like to move that piece?: ").split(","))
+		if game.movePiece(moveFrom, moveTo, colorsToMove[colorIndex]):
+			colorIndex += 1
+		else:
+			print("That was not a vailid move. Try again.")
