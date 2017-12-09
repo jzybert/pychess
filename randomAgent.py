7e57cf41928e7f8f901b4cac17a53804c0ee72d2
jzybert <jmzybert@gmail.com>
2017-12-09 12:19:34 -0500

    random agent class

---
+++ randomAgent.py
@@ -0,0 +1,18 @@
+from agent import Agent
+
+
+class RandomAgent(Agent):
+    """
+    A RandomAgent is an agent that randomly chooses a legal move to make.
+
+    :param game: a ChessGame to run the agent on
+    :param startColor: the color that the agent will choose moves for
+    """
+    def __init__(self, game, startColor):
+        super().__init__(game, startColor)
+
+    def getAction(self):
+        """
+        Randomly chooses a legal action
+        :return: the action
+        """
