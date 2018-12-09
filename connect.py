2627a9950cd1ba5d3c5b847c5bdc0e4f56c0499f
jzybert <jmzybert@gmail.com>
2018-12-09 13:40:52 -0500

    Add piece value to the chess api

--- connect.py
+++ connect.py
@@ -1,17 +1,27 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 
-@app.route("/can_move", methods=["POST", "OPTIONS"])
-def can_move():
-    req = request.get_json()
-    print(req)
-    if req and "test" in req:
-        print(req["test"])
-    return jsonify("ACK")
+@app.route("/")
+def index():
+    return 'Hello'
+
+
+@app.route("/v1/piece/value/<string:piece_name>", methods=["GET"])
+def get_piece_value(piece_name):
+    piece_name = piece_name.lower()
+    if piece_name == "king":
+        return jsonify({"piece": piece_name, "value": 100000000})
+    if piece_name == "queen":
+        return jsonify({"piece": piece_name, "value": 9})
+    if piece_name == "rook":
+        return jsonify({"piece": piece_name, "value": 5})
+    if piece_name == "bishop" or piece_name == "knight":
+        return jsonify({"piece": piece_name, "value": 3})
+    if piece_name == "pawn":
+        return jsonify({"piece": piece_name, "value": 1})
+    return jsonify({"piece": piece_name, "value": -1})
 
 
 if __name__ == "__main__":
