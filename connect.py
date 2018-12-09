cd169c9d4d4f4fd05ca8abd626950c5eb68f29b7
jzybert <jmzybert@gmail.com>
2018-12-09 13:45:31 -0500

    Clean up get_piece_value code

--- connect.py
+++ connect.py
@@ -11,17 +11,21 @@ def index():
 @app.route("/v1/piece/value/<string:piece_name>", methods=["GET"])
 def get_piece_value(piece_name):
     piece_name = piece_name.lower()
+
+    def format_json(value):
+        return jsonify({"piece": piece_name, "value": value})
+
     if piece_name == "king":
-        return jsonify({"piece": piece_name, "value": 100000000})
+        return format_json(100000000)
     if piece_name == "queen":
-        return jsonify({"piece": piece_name, "value": 9})
+        return format_json(9)
     if piece_name == "rook":
-        return jsonify({"piece": piece_name, "value": 5})
+        return format_json(5)
     if piece_name == "bishop" or piece_name == "knight":
-        return jsonify({"piece": piece_name, "value": 3})
+        return format_json(3)
     if piece_name == "pawn":
-        return jsonify({"piece": piece_name, "value": 1})
-    return jsonify({"piece": piece_name, "value": -1})
+        return format_json(1)
+    return format_json(-1)
 
 
 if __name__ == "__main__":
